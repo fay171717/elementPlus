bf23fd1873e70bd6793d5d8ffd697c711a41bb3d
fay171717 <15777063880@163.com>
2024-09-29 20:48:14 +0800

    编写 components 和 utils 的测试案例

--- packages/core/vite.es.config.ts
+++ packages/core/vite.es.config.ts
@@ -1,13 +1,14 @@
-import {defineConfig, Terser} from "vite"
+import { defineConfig, Terser } from "vite";
 import vue from "@vitejs/plugin-vue";
-import {resolve} from "path"
+import { resolve } from "path";
 import { readdirSync, readdir } from "fs";
 import { delay, defer, filter, map } from "lodash-es";
-import dts from "vite-plugin-dts"
-import shell from 'shelljs'
+import dts from "vite-plugin-dts";
+import shell from "shelljs";
 import { readFileSync } from "fs";
 import hooks from "./hooksPlugin";
-import terser from '@rollup/plugin-terser'
+import terser from "@rollup/plugin-terser";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const TRY_MOVE_STYLES_DELAY = 800 as const;
 const isProd = process.env.NODE_ENV === "production";
@@ -22,118 +23,118 @@ function moveStyles() {
 }
 
 function getDirectoriesSync(basePath: string) {
-    const entries = readdirSync(basePath, { withFileTypes: true });
-  
-    return map(
-      filter(entries, (entry) => entry.isDirectory()),
-      (entry) => entry.name
-    );
-  }
+  const entries = readdirSync(basePath, { withFileTypes: true });
+
+  return map(
+    filter(entries, (entry) => entry.isDirectory()),
+    (entry) => entry.name
+  );
+}
 
 export default defineConfig({
-    plugins:[
-        vue(),
-        dts({
-            tsconfigPath:"../../tsconfig.build.json",
-            outDir:"dist/types"
-        }),
-        hooks({
-          rmFiles:['./dist/es','./dist/theme','./dist/types'],
-          afterBuild:moveStyles
-        }),
-        terser({
-          compress: {
-            //分号
-            sequences: isProd,
-            //参数
-            arguments: isProd,
-            //log删掉
-            drop_console: isProd && ["log"],
-            drop_debugger: isProd,
-            //压缩的次数
-            passes: isProd ? 4 : 1,
-            global_defs: {
-              "@DEV": JSON.stringify(isDev),
-              "@PROD": JSON.stringify(isProd),
-              "@TEST": JSON.stringify(isTest),
-            },
-          },
-          format: {
-            semicolons: false,
-            shorthand: isProd,
-            braces: !isProd,
-            beautify: !isProd,
-            comments: !isProd,
-          },
-          mangle: {
-            toplevel: isProd,
-            eval: isProd,
-            keep_classnames: isDev,
-            
-            keep_fnames: isDev,
-          },
-        })
-        
-    ],
-    
-    build:{
-        outDir:'dist/es',
-        cssCodeSplit:true,
-        minify:false,
-        lib:{
-            entry:resolve(__dirname,"./index.ts"),
-            name:"elementPlus",
-            fileName:"index",
-            formats:["es"],
-    },
-    rollupOptions:{
-        external:[
-            "vue",
-            "@fortawesome/fontawesome-svg-core",
-            "@fortawesome/free-solid-svg-icons",
-            "@fortawesome/vue-fontawesome",
-            "@popperjs/core",
-            "async-validator",
-        ],
-
-        output:{
-            assetFileNames:(assetInfo)=>{
-                if(assetInfo.name === "style.css") return "index.css";
-                if(
-                  assetInfo.type === "asset" &&
-                  /\.(css)$/i.test(assetInfo.name as string)
-                ){
-                  return 'theme/[name].[ext]'
-                }
-                return assetInfo.name as string;
-            },
-            //分包
-            manualChunks(id){
-                if (id.includes("node_modules")) {
-                    return "vendor";
-                  }
-                  if (id.includes("/packages/hooks")) {
-                    return "hooks";
-                  }
-                  if (
-                    id.includes("/packages/utils") ||
-                    id.includes("plugin-vue:export-helper")
-                  )
-                   return "utils";
-                
-                if(id.includes("plugin-vue:export-helper")){
-                  return "test"
-                }
-                  for (const dirName of getDirectoriesSync("../components")) {
-                    if (id.includes(`/packages/components/${dirName}`)) {
-                      return dirName;
-                    }
-                  }
-            }
+  plugins: [
+    vue(),
+    //分析打包结果
+    visualizer({
+      filename: "dist/stats.es.html",
+    }),
+    dts({
+      tsconfigPath: "../../tsconfig.build.json",
+      outDir: "dist/types",
+    }),
+    hooks({
+      rmFiles: ["./dist/es", "./dist/theme", "./dist/types"],
+      afterBuild: moveStyles,
+    }),
+    terser({
+      compress: {
+        //分号
+        sequences: isProd,
+        //参数
+        arguments: isProd,
+        //log删掉
+        drop_console: isProd && ["log"],
+        drop_debugger: isProd,
+        //压缩的次数
+        passes: isProd ? 4 : 1,
+        global_defs: {
+          "@DEV": JSON.stringify(isDev),
+          "@PROD": JSON.stringify(isProd),
+          "@TEST": JSON.stringify(isTest),
         },
-        
-    }
-    
-    }
-    
-})
+      },
+      format: {
+        semicolons: false,
+        shorthand: isProd,
+        braces: !isProd,
+        beautify: !isProd,
+        comments: !isProd,
+      },
+      mangle: {
+        toplevel: isProd,
+        eval: isProd,
+        keep_classnames: isDev,
+
+        keep_fnames: isDev,
+      },
+    }),
+  ],
+
+  build: {
+    outDir: "dist/es",
+    cssCodeSplit: true,
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, "./index.ts"),
+      name: "elementPlus",
+      fileName: "index",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: [
+        "vue",
+        "@fortawesome/fontawesome-svg-core",
+        "@fortawesome/free-solid-svg-icons",
+        "@fortawesome/vue-fontawesome",
+        "@popperjs/core",
+        "async-validator",
+      ],
+
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") return "index.css";
+          if (
+            assetInfo.type === "asset" &&
+            /\.(css)$/i.test(assetInfo.name as string)
+          ) {
+            return "theme/[name].[ext]";
+          }
+          return assetInfo.name as string;
+        },
+        //分包
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          if (id.includes("/packages/hooks")) {
+            return "hooks";
+          }
+          if (
+            id.includes("/packages/utils") ||
+            id.includes("plugin-vue:export-helper")
+          )
+            return "utils";
+
+          if (id.includes("plugin-vue:export-helper")) {
+            return "test";
+          }
+          for (const dirName of getDirectoriesSync("../components")) {
+            if (id.includes(`/packages/components/${dirName}`)) {
+              return dirName;
+            }
+          }
+        },
+      },
+    },
+  },
+});
--- packages/core/vite.umd.config.ts
+++ packages/core/vite.umd.config.ts
@@ -1,72 +1,74 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import {compression} from 'vite-plugin-compression2'
-import {resolve} from "path"
+import { compression } from "vite-plugin-compression2";
+import { resolve } from "path";
 import { readFileSync } from "fs";
-import shell from "shelljs"
+import shell from "shelljs";
 import { delay } from "lodash-es";
-import hooks from "./hooksPlugin"
+import hooks from "./hooksPlugin";
 import terser from "@rollup/plugin-terser";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const TRY_MOVE_STYLES_DELAY = 800 as const;
 const isProd = process.env.NODE_ENV === "production";
 const isDev = process.env.NODE_ENV === "development";
 const isTest = process.env.NODE_ENV === "test";
 
-function moveStyles(){
-    try{
-        readFileSync('./dist/umd/index.css.gz');
-        shell.cp("./dist/umd/index.css","./dist/index.css")
-    }catch(_){
-        delay(moveStyles,TRY_MOVE_STYLES_DELAY)
-    }
+function moveStyles() {
+  try {
+    readFileSync("./dist/umd/index.css.gz");
+    shell.cp("./dist/umd/index.css", "./dist/index.css");
+  } catch (_) {
+    delay(moveStyles, TRY_MOVE_STYLES_DELAY);
+  }
 }
 
 export default defineConfig({
-    plugins:[
-        vue(),
-        compression({
-            include:/.(cjs|css)$/i
-        }),
-        terser({
-            compress: {
-              drop_console: ["log"],
-              drop_debugger: true,
-              passes: 3,
-              global_defs: {
-                "@DEV": JSON.stringify(isDev),
-                "@PROD": JSON.stringify(isProd),
-                "@TEST": JSON.stringify(isTest),
-              },
-            },
-          }),
-        hooks({
-            rmFiles:['./dist/umd','./dist/index.css'],
-            afterBuild:moveStyles
-        })
-    ],
-    build:{
-        outDir:'dist/umd',
-        lib:{
-            entry:resolve(__dirname,"./index.ts"),
-            name:"elementPlus",
-            fileName:"index",
-            formats:["umd"],
+  plugins: [
+    vue(),
+    visualizer({
+      filename: "dist/stats.umd.html",
+    }),
+    compression({
+      include: /.(cjs|css)$/i,
+    }),
+    terser({
+      compress: {
+        drop_console: ["log"],
+        drop_debugger: true,
+        passes: 3,
+        global_defs: {
+          "@DEV": JSON.stringify(isDev),
+          "@PROD": JSON.stringify(isProd),
+          "@TEST": JSON.stringify(isTest),
+        },
+      },
+    }),
+    hooks({
+      rmFiles: ["./dist/umd", "./dist/index.css"],
+      afterBuild: moveStyles,
+    }),
+  ],
+  build: {
+    outDir: "dist/umd",
+    lib: {
+      entry: resolve(__dirname, "./index.ts"),
+      name: "elementPlus",
+      fileName: "index",
+      formats: ["umd"],
     },
-    rollupOptions:{
-        external:["vue"],
-        output:{
-            exports:"named",
-            globals:{
-                vue:"Vue"
-            },
-            assetFileNames:(assetInfo)=>{
-                if(assetInfo.name === "style.css") return "index.css";
-                return assetInfo.name as string;
-            }
-        }
-    }
-    
-    }
-    
-})
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css") return "index.css";
+          return assetInfo.name as string;
+        },
+      },
+    },
+  },
+});
